44a043ecf0a28fb83f83528ba3cb8627cfa58757
juuliana <jumaju10@hotmail.com>
2023-09-16 19:04:53 -0300

    feat: create generateAiCompletion function

---
+++ server/src/routes/generate-ai-completion.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { FastifyInstance } from "fastify";
+
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+
+export async function generateAiCompletionRoute(app: FastifyInstance) {
+  app.post("/ai/complete", async (request, reply) => {
+    const boydSchema = z.object({
+      videoId: z.string().uuid(),
+      template: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
+    });
+
+    const { videoId, template, temperature } = boydSchema.parse(request.body);
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video.transcription) {
+      return reply
+        .status(400)
+        .send({ error: "A transcrição do vídeo ainda não foi gerada." });
+    }
+
+    const promptMessage = template.replace(
+      "{transcription}",
+      video.transcription
+    );
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-16k",
+      temperature,
+      messages: [{ content: promptMessage, role: "user" }],
+    });
+
+    return response;
+  });
+}
